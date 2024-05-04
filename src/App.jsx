35aeaa9759e9bc76a005cea11bfff6133b006948
Prateek Giri <prateek.giri@conceptext.com>
2024-05-04 11:02:32 +0530

    project - 1

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+import Navigation from "./component/Nevigation";
+import HeroSection from "./component/Hero";
+const App = () =>{
+   return (
+    <>
+    <Navigation></Navigation>
+    <HeroSection></HeroSection>
+    </>
+   )
+};
+export default App;
